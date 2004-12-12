1e0a6bd0bbf6909a9505944d6d65dce514b12cfc
Dave Townsend <dtownsend@oxymoronical.com>
2004-12-12 22:18:00 +0000

    Property searching for duplicates. Doesnt work, probably because of bad properties to compare, like modification times.

--- CompareAddin/CompareResults.cs
+++ CompareAddin/CompareResults.cs
@@ -10,6 +10,7 @@ using System.Drawing;
 using System.Collections;
 using System.ComponentModel;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Outlook;
 
 namespace CompareAddin
 {
@@ -21,7 +22,36 @@ namespace CompareAddin
 		private System.ComponentModel.Container components = null;
 
 		private IDictionary cache;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Splitter splitter1;
+		private System.Windows.Forms.Panel panel2;
 		private ItemPropertyHandler props;
+
+		private class OutlookItem
+		{
+			private object item;
+			private ItemPropertyHandler props;
+
+			public OutlookItem(object item, ItemPropertyHandler props)
+			{
+				this.item=item;
+				this.props=props;
+			}
+
+			public void Display()
+			{
+				if (item is ContactItem)
+				{
+					ContactItem oitem = (ContactItem)item;
+					oitem.Display(false);
+				}
+			}
+
+			public override string ToString()
+			{
+				return props.FetchFolder(item).FullFolderPath+" ("+props.FetchNameProperty(item)+")";
+			}
+		}
 
 		public CompareResults(IList duplicates, IDictionary cache, ItemPropertyHandler props)
 		{
@@ -58,50 +88,83 @@ namespace CompareAddin
 			this.listItems = new System.Windows.Forms.ListBox();
 			this.listDuplicates = new System.Windows.Forms.ListBox();
 			this.btnOK = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.splitter1 = new System.Windows.Forms.Splitter();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// listItems
 			// 
-			this.listItems.Location = new System.Drawing.Point(8, 8);
+			this.listItems.Dock = System.Windows.Forms.DockStyle.Left;
+			this.listItems.Location = new System.Drawing.Point(0, 0);
 			this.listItems.Name = "listItems";
-			this.listItems.Size = new System.Drawing.Size(240, 303);
+			this.listItems.Size = new System.Drawing.Size(136, 368);
 			this.listItems.TabIndex = 0;
-			this.listItems.SelectedValueChanged += new System.EventHandler(this.listItems_SelectedValueChanged);
 			this.listItems.SelectedIndexChanged += new System.EventHandler(this.listItems_SelectedIndexChanged);
 			// 
 			// listDuplicates
 			// 
-			this.listDuplicates.Location = new System.Drawing.Point(256, 8);
+			this.listDuplicates.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listDuplicates.Location = new System.Drawing.Point(0, 0);
 			this.listDuplicates.Name = "listDuplicates";
-			this.listDuplicates.Size = new System.Drawing.Size(320, 303);
+			this.listDuplicates.Size = new System.Drawing.Size(544, 368);
 			this.listDuplicates.TabIndex = 1;
+			this.listDuplicates.DoubleClick += new System.EventHandler(this.listDuplicates_DoubleClick);
 			// 
 			// btnOK
 			// 
-			this.btnOK.Location = new System.Drawing.Point(496, 328);
+			this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnOK.Location = new System.Drawing.Point(456, 16);
 			this.btnOK.Name = "btnOK";
 			this.btnOK.TabIndex = 2;
 			this.btnOK.Text = "OK";
 			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.splitter1);
+			this.panel1.Controls.Add(this.listItems);
+			this.panel1.Controls.Add(this.listDuplicates);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(544, 379);
+			this.panel1.TabIndex = 3;
+			// 
+			// splitter1
+			// 
+			this.splitter1.Location = new System.Drawing.Point(136, 0);
+			this.splitter1.Name = "splitter1";
+			this.splitter1.Size = new System.Drawing.Size(3, 379);
+			this.splitter1.TabIndex = 1;
+			this.splitter1.TabStop = false;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.btnOK);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel2.Location = new System.Drawing.Point(0, 323);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(544, 56);
+			this.panel2.TabIndex = 4;
+			// 
 			// CompareResults
 			// 
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.ClientSize = new System.Drawing.Size(584, 363);
-			this.Controls.Add(this.btnOK);
-			this.Controls.Add(this.listDuplicates);
-			this.Controls.Add(this.listItems);
+			this.ClientSize = new System.Drawing.Size(544, 379);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "CompareResults";
 			this.Text = "Results of Comparison";
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 		#endregion
-
-		private void listItems_SelectedValueChanged(object sender, System.EventArgs e)
-		{
-		}
 
 		private void listItems_SelectedIndexChanged(object sender, System.EventArgs e)
 		{
@@ -110,8 +173,13 @@ namespace CompareAddin
 			IList items = (IList)cache[key];
 			foreach (object obj in items)
 			{
-				listDuplicates.Items.Add(props.FetchFolder(obj).FullFolderPath+" ("+props.FetchNameProperty(obj)+")");
+				listDuplicates.Items.Add(new OutlookItem(obj,props));
 			}
+		}
+
+		private void listDuplicates_DoubleClick(object sender, System.EventArgs e)
+		{
+			((OutlookItem)listDuplicates.SelectedItem).Display();
 		}
 	}
 }
--- CompareAddin/CompareThread.cs
+++ CompareAddin/CompareThread.cs
@@ -15,6 +15,73 @@ namespace CompareAddin
 {
 	public class CompareThread
 	{
+		private string[] propertyList = 
+			{
+				"Account","Actions","Anniversary",
+				"Application","AssistantName",
+				"AssistantTelephoneNumber","Attachments",
+"AutoResolvedWinner","BillingInformation","Birthday",
+"Body","Business2TelephoneNumber","BusinessAddress",
+"BusinessAddressCity","BusinessAddressCountry",
+				"BusinessAddressPostalCode","BusinessAddressPostOfficeBox",
+"BusinessAddressState","BusinessAddressStreet",
+"BusinessFaxNumber","BusinessHomePage",
+"BusinessTelephoneNumber","CallbackTelephoneNumber",
+"CarTelephoneNumber",
+				"Categories","Children","Class",
+"Companies","CompanyAndFullName",
+"CompanyLastFirstNoSpace","CompanyLastFirstSpaceOnly",
+"CompanyMainTelephoneNumber","CompanyName",
+"ComputerNetworkName","Conflicts","ConversationIndex",
+"ConversationTopic","CreationTime","CustomerID",
+"Department","DownloadState","Email1Address",
+"Email1AddressType","Email1DisplayName","Email1EntryID",
+"Email2Address",
+				"Email2AddressType","Email2DisplayName",
+"Email2EntryID","Email3Address","Email3AddressType",
+"Email3DisplayName","Email3EntryID","EntryID",
+"FileAs","FirstName","FormDescription",
+"FTPSite","FullName","FullNameAndCompany",
+"Gender","GetInspector","GovernmentIDNumber",
+"HasPicture","Hobby","Home2TelephoneNumber",
+"HomeAddress","HomeAddressCity",
+				"HomeAddressCountry","HomeAddressPostalCode",
+"HomeAddressPostOfficeBox","HomeAddressState",
+"HomeAddressStreet","HomeFaxNumber",
+"HomeTelephoneNumber","IMAddress","Importance",
+"Initials","InternetFreeBusyAddress","IsConflict",
+"ISDNNumber","ItemProperties","JobTitle",
+"Journal","Language","LastFirstAndSuffix",
+"LastFirstNoSpace","LastFirstNoSpaceAndSuffix",
+"LastFirstNoSpaceCompany",
+				"LastFirstSpaceOnly","LastFirstSpaceOnlyCompany",
+"LastModificationTime","LastName",
+"LastNameAndFirstName","Links","MailingAddress",
+"MailingAddressCity","MailingAddressCountry",
+"MailingAddressPostalCode","MailingAddressPostOfficeBox",
+"MailingAddressState","MailingAddressStreet",
+"ManagerName","MarkForDownload","MessageClass",
+"MiddleName","Mileage","MobileTelephoneNumber",
+"NetMeetingAlias",
+				"NetMeetingServer","NickName","NoAging",
+"OfficeLocation","OrganizationalIDNumber","OtherAddress",
+"OtherAddressCity","OtherAddressCountry",
+"OtherAddressPostalCode","OtherAddressPostOfficeBox",
+"OtherAddressState","OtherAddressStreet",
+"OtherFaxNumber","OtherTelephoneNumber",
+"OutlookInternalVersion","OutlookVersion",
+"PagerNumber","Parent","PersonalHomePage",
+"PrimaryTelephoneNumber","Profession",
+				"RadioTelephoneNumber","ReferredBy",
+"Saved","SelectedMailingAddress","Sensitivity",
+"Session","Size","Spouse","Subject",
+"Suffix","TelexNumber","Title",
+"TTYTDDTelephoneNumber","UnRead","User1",
+"User2","User3","User4","UserCertificate",
+"UserProperties","WebPage","YomiCompanyName",
+"YomiFirstName","YomiLastName"
+			};
+
 		private Microsoft.Office.Interop.Outlook.Application application;
 
 		public CompareThread(Microsoft.Office.Interop.Outlook.Application application)
@@ -25,7 +92,7 @@ namespace CompareAddin
 			Run();
 		}
 
-		private void DoCompare(IList duplicateList, IDictionary cache, ItemPropertyHandler props, object obj)
+		private void DoCompare(IList redundant, IList duplicateList, IDictionary cache, ItemPropertyHandler props, object obj)
 		{
 			if (props.IsCorrectType(obj))
 			{
@@ -42,7 +109,53 @@ namespace CompareAddin
 						list = new ArrayList();
 						cache.Add(value,list);
 					}
-					list.Add(obj);
+					bool exact=false;
+					UserProperties testprops = props.FetchUserProperties(obj);
+					foreach (object known in list)
+					{
+						exact=true;
+						UserProperties knownprops = props.FetchUserProperties(known);
+						foreach (string prop in propertyList)
+						{
+							string knownprop = null;
+							try
+							{
+								UserProperty check = knownprops.Find(prop,false);
+								if (check!=null)
+								{
+									knownprop = (string)check.Value;
+								}
+							}
+							catch {}
+							string testprop = null;
+							try
+							{
+								UserProperty check = testprops.Find(prop,false);
+								if (check!=null)
+								{
+									testprop = (string)check.Value;
+								}
+							}
+							catch {}
+							if ((testprop!=knownprop))
+							{
+								exact=false;
+								break;
+							}
+						}
+						if (exact)
+						{
+							break;
+						}
+					}
+					if (!exact)
+					{
+						list.Add(obj);
+					}
+					else
+					{
+						redundant.Add(obj);
+					}
 					if (list.Count==2)
 					{
 						duplicateList.Add(value);
@@ -60,24 +173,27 @@ namespace CompareAddin
 
 				IDictionary cache = new Hashtable();
 				IList duplicateList = new ArrayList();
+				IList redundant = new ArrayList();
 
 				ProgressDialog progress = new ProgressDialog("Scanning Folders","Scanning folders. Please Wait.");
 				progress.Value=0;
 				progress.Maximum=options.Folder1.Items.Count;
+				if (options.CompareMultipleFolders)
+				{
+					progress.Maximum+=options.Folder2.Items.Count;
+				}
 				progress.Show();
 				foreach (object obj in options.Folder1.Items)
 				{
-					DoCompare(duplicateList,cache,props,obj);
+					DoCompare(redundant,duplicateList,cache,props,obj);
 					progress.Value++;
 				}
 
 				if (options.CompareMultipleFolders)
 				{
-					progress.Value=0;
-					progress.Maximum=options.Folder2.Items.Count;
 					foreach (object obj in options.Folder2.Items)
 					{
-						DoCompare(duplicateList,cache,props,obj);
+						DoCompare(redundant,duplicateList,cache,props,obj);
 						progress.Value++;
 					}
 				}
@@ -85,6 +201,7 @@ namespace CompareAddin
 				progress.Close();
 				progress.Dispose();
 
+				MessageBox.Show("Found "+redundant.Count+" exact duplicates");
 				CompareResults results = new CompareResults(duplicateList,cache,props);
 				results.Show();
 			}
